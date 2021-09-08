e6a0d37dd755f42116b9d254bae4405d552bfd73
Chetanshidling1999 <89764315+Chetanshidling1999@users.noreply.github.com>
2021-09-08 12:47:55 +0530

    Delete MiniProject_C directory

--- MiniProject_C/3_Implementation/ElectricityBill.h
+++
@@ -1,7 +0,0 @@
-#ifndef __ElectricityBill_H__
-#define __ElectricityBill_H__
-
-void Urban();
-void Rural();
-
-#endif  
--- MiniProject_C/3_Implementation/tempCodeRunnerFile.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int a, sum;
-    printf("Enter number");
-    scanf("%d",&a);
-    sum = a + a;
-    printf("Sum: %d", sum);
-}
